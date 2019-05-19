6f4d87dc318f4808e4f2ed1230cafb5fb85e6179
Marta Trojanowska <m.troyanovska@gmail.com>
2019-05-19 20:49:12 +0200

    fixed conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_05_12_065334) do
-=======
-
->>>>>>> schema
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,17 +30,9 @@ ActiveRecord::Schema.define(version: 2019_05_12_065334) do
     t.string "duration", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.bigint "artist_id", null: false
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
   add_foreign_key "songs", "artists"
 end
-=======
-
-    t.bigint "artist_id", null: false
-    t.index ["artist_id"], name: "index_songs_on_artist_id"
-
-  end
->>>>>>> schema
