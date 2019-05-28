2116438b2eb56b43e8f7f265dbe5abcfb1393920
Marta Trojanowska <m.troyanovska@gmail.com>
2019-05-28 22:29:02 +0200

    birthdate validation, released validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,14 @@ class Artist < ApplicationRecord
   validates :nickname, presence: true
   validates :nickname, uniqueness: {case_sensitive: false}
   validates :birthdate, presence: true
+  validate :birthdate_in_the_future
   validates :origin, presence: true
   validates :biography, presence: true, length: { minimum: 250 }
+end
+
+
+def birthdate_in_the_future
+  if birthdate > Date.today
+    errors.add(:birthdate, "can't be in the future")
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,26 @@
+
 class Song < ApplicationRecord
+
   validates :title, presence: true, uniqueness: true
-  DATE_REGEX = /\A\d{4}-\d{2}-\d{2}\z/
-  validates :released, presence: true, format: { with: DATE_REGEX }
+
+  validate :future_release_time
+  validate :past_release_time
+  validates :released, presence: true
+
   validates :duration, presence: true
 end
+
+    def future_release_time
+      if released > Time.now
+      errors.add(:released, "is the future date")
+    end
+  end
+
+  def past_release_time
+    today = Date.today
+    current_year = today.year
+    released_year = released.year
+    if released_year < current_year - 90
+      errors.add(:released, "- too early for records")
+    end
+  end
