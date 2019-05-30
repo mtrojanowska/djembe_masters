dc493eb621ade7f2d1a45fa9ae1d32ec4e6a893a
Marta Trojanowska <m.troyanovska@gmail.com>
2019-05-30 22:38:19 +0200

    released validation in song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,26 +1,21 @@
+# frozen_string_literal: true
 
 class Song < ApplicationRecord
-
   validates :title, presence: true, uniqueness: true
-
   validate :future_release_time
   validate :past_release_time
   validates :released, presence: true
-
   validates :duration, presence: true
 end
 
-    def future_release_time
-      if released > Time.now
-      errors.add(:released, "is the future date")
-    end
+def future_release_time
+  errors.add(:released, 'is the future date') if released > Time.now
   end
 
-  def past_release_time
-    today = Date.today
-    current_year = today.year
-    released_year = released.year
-    if released_year < current_year - 90
-      errors.add(:released, "- too early for records")
-    end
+def past_release_time
+  first_record = 1929
+  released_year = released.year
+  if released_year < first_record
+    errors.add(:released, '- too early for records')
   end
+end
