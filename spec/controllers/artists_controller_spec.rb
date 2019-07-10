29d1e68e8bb5cf5fcb96f0677d0afd96af90d4c0
Marta Trojanowska <m.troyanovska@gmail.com>
2019-07-10 22:11:22 +0200

    get new test in artists_controller_spec removed

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe ArtistsController, type: :controller do
   describe 'GET #index' do
     it 'checks index functionality' do
-      artist1 = create(:artist, nickname: "Jajo")
+      artist1 = create(:artist, nickname: 'Jajo')
       artist2 = create(:artist, nickname: 'Jojo')
       get :index
       expect(assigns(:artists)).to eq([artist1, artist2])
@@ -32,14 +32,9 @@ RSpec.describe ArtistsController, type: :controller do
   end
 
   describe 'GET #new' do
-    # it 'returns a success response' do
-    #   get :new
-    #   expect(assigns(:artist)).to be_a_new(Artist)
-    # end
-
     it 'returns a success response' do
       get :new
-      expect(response).to have_http_status "200"
+      expect(response).to have_http_status '200'
     end
   end
 
@@ -62,14 +57,14 @@ RSpec.describe ArtistsController, type: :controller do
   describe 'POST #create' do
     context 'with valid params' do
       it 'creates a new Artist' do
-          expect do
-          post :create, params: { artist: attributes_for(:artist)}
-          end.to change(Artist, :count).by(1)
+        expect do
+          post :create, params: { artist: attributes_for(:artist) }
+        end.to change(Artist, :count).by(1)
       end
 
       it 'redirects to the @artist' do
         expect do
-        post :create, params: { artist: attributes_for(:artist)}
+          post :create, params: { artist: attributes_for(:artist) }
         end.to change(Artist, :count).by(1)
         artist = Artist.last
         expect(response).to redirect_to(artist_path(artist))
@@ -79,7 +74,7 @@ RSpec.describe ArtistsController, type: :controller do
     context 'with invalid params' do
       it 'email and password is not enough' do
         expect do
-        post :create, params: { artist: attributes_for(:artist, nickname: nil, birthdate: nil, origin: nil, biography: nil) }
+          post :create, params: { artist: attributes_for(:artist, nickname: nil, birthdate: nil, origin: nil, biography: nil) }
         end.to_not change(Artist, :count)
         expect(response.code).to render_template :new
       end
@@ -90,7 +85,7 @@ RSpec.describe ArtistsController, type: :controller do
         it 'updates the requested artist' do
           artist = create(:artist)
           sign_in artist
-          put :update, params: { id: artist.id, artist: attributes_for(:artist, email: 'jerry@example.com', password: "password", password_confirmation: "password") }
+          put :update, params: { id: artist.id, artist: attributes_for(:artist, email: 'jerry@example.com', password: 'password', password_confirmation: 'password') }
           expect(artist.reload.email).to eq('jerry@example.com')
         end
 
@@ -102,15 +97,13 @@ RSpec.describe ArtistsController, type: :controller do
         end
       end
 
-        context 'invalid attributes' do
-          it 'updates the requested artits' do
-            artist = create(:artist)
-            sign_in artist
-            put :update, params: { id: artist.id, artist: attributes_for(:artist, nickname: '') }
-            expect(response).to render_template :new
-          end
-
-
+      context 'invalid attributes' do
+        it 'updates the requested artits' do
+          artist = create(:artist)
+          sign_in artist
+          put :update, params: { id: artist.id, artist: attributes_for(:artist, nickname: '') }
+          expect(response).to render_template :new
+        end
 
         describe 'DELETE #destroy' do
           it 'destroys the requested artist' do
@@ -128,7 +121,7 @@ RSpec.describe ArtistsController, type: :controller do
             artist2 = create(:artist, nickname: 'Jojo')
             artist3 = create(:artist, nickname: 'Boo')
             sign_in artist1
-              delete :destroy, params: { id: artist1.id }
+            delete :destroy, params: { id: artist1.id }
             expect(response).to redirect_to '/artists'
           end
         end
